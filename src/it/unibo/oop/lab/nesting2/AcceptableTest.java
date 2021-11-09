9dbcb632d075a0ee33d99805c93abd5e910770cc
Corrado <corrado.stortini2@studio.unibo.it>
2021-11-09 18:17:22 +0100

    Complete the Test class

--- src/it/unibo/oop/lab/nesting2/AcceptableTest.java
+++ src/it/unibo/oop/lab/nesting2/AcceptableTest.java
@@ -26,10 +26,8 @@ public class AcceptableTest {
          * Correct acceptance of [10, 20, 30, 40]
          */
         final List<Integer> list = Arrays.asList(10, 20, 30, 40);
-        /*
-         * the following needs instantiation
-         */
-        final Acceptable<Integer> acc = null;
+        
+        final Acceptable<Integer> acc = new OneListAcceptable<Integer>(list);
         final Acceptor<Integer> acceptor = acc.acceptor();
         for (final Integer el: list) {
             acceptor.accept(el);
@@ -47,7 +45,7 @@ public class AcceptableTest {
          * Failing acceptance
          */
         final List<Integer> list = Arrays.asList(10, 20, 30, 40);
-        final Acceptable<Integer> acc = null;
+        final Acceptable<Integer> acc = new OneListAcceptable<Integer>(list);
         final Acceptor<Integer> acceptor = acc.acceptor();
         try {
             for (final Integer el: list) {
@@ -79,7 +77,7 @@ public class AcceptableTest {
          * Exception due to early end
          */
         final List<Integer> list = Arrays.asList(10, 20, 30, 40);
-        final Acceptable<Integer> acc = null;
+        final Acceptable<Integer> acc = new OneListAcceptable<Integer>(list);
         final Acceptor<Integer> acceptor = acc.acceptor();
         try {
             acceptor.accept(10);
